57407cfe1f6be1d993336e5415c147c1a6fe0140
Beautiful-Skyline <Beautiful-Skyline@users.noreply.github.com>
2014-10-15 13:10:16 +0400

    First pages commit

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,7 +26,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Письмо отправлено. Ура! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -38,7 +38,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Что-то не так с сервером. Попробуйте позже или позвоните нам.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
